e9ac5e5f7579d5e4233e87a8f6b9d914c75bb9a5
Rain Jocas <rainjocas@gmail.com>
2024-06-06 15:23:45 -0400

    'regular links to navlinks'

--- Rain-Jocas/src/components/NavigationBar.tsx
+++ Rain-Jocas/src/components/NavigationBar.tsx
@@ -16,11 +16,10 @@ function NavigationBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Link to="/about">About </Link>
-            <Link to="/projects">Projects </Link>
-            <Link to="/resume">Resume </Link>
-            <Nav.Link href="/about">Link</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/projects">Projects</Nav.Link>
+            <Nav.Link href="/resume">Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
