0105286224fd60e5866466b58f142f19791678d4
Rain Jocas <rainjocas@gmail.com>
2024-06-06 15:08:22 -0400

    updating navbar

---
+++ Rain-Jocas/src/a/Resume.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {PageWrapper} from "../pageSections/PageWrapper";
+//import {Section} from "../pageSections/Section";
+
+const Resume: React.FC = () => {
+    return (
+      <PageWrapper>
+          <p className="text-xl font-bold underline">
+            Congratulations, you've reached the Resume page
+          </p>
+      </PageWrapper>
+    );
+  };
+  
+  export default Resume;
