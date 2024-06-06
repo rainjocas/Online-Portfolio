75e1f6dd0ad7476694f59e0c37ed72f7c4146be5
Rain Jocas <rainjocas@gmail.com>
2024-06-06 15:08:55 -0400

    updating navbar

--- Rain-Jocas/src/components/NavigationBar.tsx
+++ Rain-Jocas/src/components/NavigationBar.tsx
@@ -17,6 +17,9 @@ function NavigationBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
