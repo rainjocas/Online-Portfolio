eb3929b2e3ba256c83fe6a05a743c064a1adf8b2
Rain Jocas <rainjocas@gmail.com>
2024-06-17 11:46:10 -0400

    got rid of built in brand

--- Rain-Jocas/src/components/NavigationBar.tsx
+++ Rain-Jocas/src/components/NavigationBar.tsx
@@ -12,7 +12,6 @@ function NavigationBar() {
   return (
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
